1e20dcb513120c58e79dbf1ccee8557df31a0c51
Aji Suryawan <kazumovie132@gmail.com>
2026-05-07 09:58:41 +0700

    fix: [AS] login shared pref

--- app/src/main/java/com/example/belajarandroid10pplg2/MainActivity.java
+++ app/src/main/java/com/example/belajarandroid10pplg2/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.belajarandroid10pplg2;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,12 +19,16 @@ public class MainActivity extends AppCompatActivity {
     EditText edUsername;
     EditText edPassword;
     Button btnLogin;
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        sharedPreferences = getSharedPreferences("LOGIN_PREF", MODE_PRIVATE);
+
         // menyambungkan objek ke widget di layout xml
         edUsername = (EditText) findViewById(R.id.edUsername);
         edPassword = (EditText) findViewById(R.id.edPassword);
@@ -35,6 +40,9 @@ public class MainActivity extends AppCompatActivity {
                 String username = edUsername.getText().toString();
                 String password = edPassword.getText().toString();
                 if(username.equals("admin") && password.equals("admin")){
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putString("username", username);
+                    editor.apply();
                     //sukses
                     Toast.makeText(MainActivity.this, "sukses login", Toast.LENGTH_SHORT).show();
                     startActivity(new Intent(MainActivity.this, ListSiswa.class));
